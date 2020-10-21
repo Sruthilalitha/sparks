f79af46b2e28c20c45ce0d4eea7a1344a35168c5
Sruthilalitha <65654279+Sruthilalitha@users.noreply.github.com>
2020-10-21 17:11:48 +0530

    commit

--- task2/settings.py
+++ task2/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 STATICROOT=os.path.join(BASE_DIR, "static")
